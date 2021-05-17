e67079b09e739ca2d7f666a2a6107b788d788d0c
ErenBtrk <erenbtrk@hotmail.com>
2021-05-17 15:02:58 +0300

    Exercise 4

--- Exercise4.py
+++ Exercise4.py
@@ -11,12 +11,17 @@
 *
 '''
 
-n= 10
+n= 3
 
 
-for j in range(1,n+1):
-    print('*'*j)
-i = n-1
-for j in range(n,0,-1):
-    print('*'*i)
-    i-=1
+for i in range(n):
+    for j in range(i):
+        print('*',end='')
+    print('')
+
+for i in range(n,0,-1):
+    for j in range(i):
+        print('*',end='')
+    print('')
+
+

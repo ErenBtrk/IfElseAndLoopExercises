f7ce1f788053761b5fbb527b560d83465d0260a2
= <=>
2021-05-21 22:30:04 +0300

    Exercise 20

---
+++ Exercise20.py
@@ -0,0 +1,26 @@
+'''
+20. Write a Python program to print alphabet pattern 'G'.
+Expected Output:
+
+  ***                                                                   
+ *   *                                                                  
+ *                                                                      
+ * ***                                                                  
+ *   *                                                                  
+ *   *                                                                  
+  *** 
+'''
+
+str = ""
+
+for row in range(7):
+    if(row == 0 or row == 6):
+        str += " ***\n"
+    elif(row == 2):
+        str += "*\n"
+    elif(row == 3):
+        str += "* ***\n"
+    else:
+        str += "*   *\n"
+
+print(str)

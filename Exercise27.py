82f764504688c7b8558668811e8037fdcbcc75e9
= <=>
2021-05-22 19:17:55 +0300

    Exercise 27

---
+++ Exercise27.py
@@ -0,0 +1,22 @@
+'''
+28. Write a Python program to print alphabet pattern 'U'.
+Expected Output:
+
+ *   *                                                                  
+ *   *                                                                  
+ *   *                                                                  
+ *   *                                                                  
+ *   *                                                                  
+ *   *                                                                  
+  *** 
+'''
+
+str = ""
+
+for row in range(7):
+    if(row == 6):
+        str += " *** \n"
+    else:
+        str += "*   *\n"
+
+print(str)

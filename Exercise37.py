5e24e059af119e16c4cce488a6f2ccaaf9433417
= <=>
2021-05-23 22:44:16 +0300

    Exercise 37

--- Exercise37.py
+++ Exercise37.py
@@ -9,7 +9,7 @@ Season is autumn
 '''
 
 months = ["january","february","march","april","may","june",
-          "july","august","september","october","november","september","december"]
+          "july","august","september","october","november","december"]
 
 monthDays = [31,29,31,30,31,30,31,31,30,31,30,31]
 
